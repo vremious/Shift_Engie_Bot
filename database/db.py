4fb5e6e03d058877cfbfc0edceea6b318380cda6
Denis Emelyanov <101509271+vremious@users.noreply.github.com>
2024-02-06 11:15:27 +0300

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,9 +1,10 @@
 import sqlite3 as sq
 
+#Определяем переменные для БД Sqlite3
 db = sq.connect('tg.db')
 cur = db.cursor()
 
-
+#Создаём БД с таблицей (если БД не существует)
 async def db_start():
     cur.execute("CREATE TABLE IF NOT EXISTS accounts("
                 "id INTEGER PRIMARY KEY AUTOINCREMENT, "
@@ -17,6 +18,7 @@ async def db_start():
     db.commit()
 
 
+#Функция для запроса данных пользователя по его telegram id. В случае если пользователя не сущестует - создаётся новая запись в таблицу (регистрация)
 async def cmd_start_db(user_id):
     user = cur.execute("SELECT * FROM accounts WHERE tg_id == {key}".format(key=user_id)).fetchone()
     if not user:
@@ -24,23 +26,25 @@ async def cmd_start_db(user_id):
         db.commit()
 
 
+#Удалённый функционал, позволявший проверять авторизацию пользователя по паролю при первом запуске бота 
 async def cmd_authorize(user_id):
     cur.execute("UPDATE accounts SET is_auth = True WHERE tg_id == {key}".format(key=user_id))
     db.commit()
 
 
+#Запись табельного номера пользователя (выполняется в машине состояний user handlers)
 async def add_tabel(user_id, message):
     cur.execute("UPDATE accounts SET tabel=({tabel}) WHERE tg_id == {user_id}".format(tabel=message,
                                                                                       user_id=user_id))
     db.commit()
 
-
+#Изменение состояния напоминаний (машина состояний в user handlers)
 async def add_notifications(user_id, message):
     cur.execute("UPDATE accounts SET notifications=({notif}) WHERE tg_id == {user_id}".format(notif=message,
                                                                                               user_id=user_id))
     db.commit()
 
-
+#Изменение времени напоминаний (машина состояний в user handlers)
 async def add_notifications_time(user_id, message):
     cur.execute("UPDATE accounts SET notif_time='{time}' WHERE tg_id == {user_id}".format(time=message,
                                                                                           user_id=user_id))
